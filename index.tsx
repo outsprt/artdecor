9d7f34e83bc5c5cbae2c18d2e4a3c1e80c283b2b
outsprt <150111835+outsprt@users.noreply.github.com>
2025-09-16 21:36:59 +0500

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,116 @@
+/**
+ * @fileoverview Control real time music with a MIDI controller
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type { PlaybackState, Prompt } from './types';
+import { GoogleGenAI, LiveMusicFilteredPrompt } from '@google/genai';
+import { PromptDjMidi } from './components/PromptDjMidi';
+import { ToastMessage } from './components/ToastMessage';
+import { LiveMusicHelper } from './utils/LiveMusicHelper';
+import { AudioAnalyser } from './utils/AudioAnalyser';
+
+const ai = new GoogleGenAI({ apiKey: process.env.API_KEY, apiVersion: 'v1alpha' });
+const model = 'lyria-realtime-exp';
+
+function main() {
+  const initialPrompts = buildInitialPrompts();
+
+  const pdjMidi = new PromptDjMidi(initialPrompts);
+  document.body.appendChild(pdjMidi);
+
+  const toastMessage = new ToastMessage();
+  document.body.appendChild(toastMessage);
+
+  const liveMusicHelper = new LiveMusicHelper(ai, model);
+  liveMusicHelper.setWeightedPrompts(initialPrompts);
+
+  const audioAnalyser = new AudioAnalyser(liveMusicHelper.audioContext);
+  liveMusicHelper.extraDestination = audioAnalyser.node;
+
+  pdjMidi.addEventListener('prompts-changed', ((e: Event) => {
+    const customEvent = e as CustomEvent<Map<string, Prompt>>;
+    const prompts = customEvent.detail;
+    liveMusicHelper.setWeightedPrompts(prompts);
+  }));
+
+  pdjMidi.addEventListener('play-pause', () => {
+    liveMusicHelper.playPause();
+  });
+
+  liveMusicHelper.addEventListener('playback-state-changed', ((e: Event) => {
+    const customEvent = e as CustomEvent<PlaybackState>;
+    const playbackState = customEvent.detail;
+    pdjMidi.playbackState = playbackState;
+    playbackState === 'playing' ? audioAnalyser.start() : audioAnalyser.stop();
+  }));
+
+  liveMusicHelper.addEventListener('filtered-prompt', ((e: Event) => {
+    const customEvent = e as CustomEvent<LiveMusicFilteredPrompt>;
+    const filteredPrompt = customEvent.detail;
+    toastMessage.show(filteredPrompt.filteredReason!)
+    pdjMidi.addFilteredPrompt(filteredPrompt.text!);
+  }));
+
+  const errorToast = ((e: Event) => {
+    const customEvent = e as CustomEvent<string>;
+    const error = customEvent.detail;
+    toastMessage.show(error);
+  });
+
+  liveMusicHelper.addEventListener('error', errorToast);
+  pdjMidi.addEventListener('error', errorToast);
+
+  audioAnalyser.addEventListener('audio-level-changed', ((e: Event) => {
+    const customEvent = e as CustomEvent<number>;
+    const level = customEvent.detail;
+    pdjMidi.audioLevel = level;
+  }));
+
+}
+
+function buildInitialPrompts() {
+  // Pick 3 random prompts to start at weight = 1
+  const startOn = [...DEFAULT_PROMPTS]
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 3);
+
+  const prompts = new Map<string, Prompt>();
+
+  for (let i = 0; i < DEFAULT_PROMPTS.length; i++) {
+    const promptId = `prompt-${i}`;
+    const prompt = DEFAULT_PROMPTS[i];
+    const { text, color } = prompt;
+    prompts.set(promptId, {
+      promptId,
+      text,
+      weight: startOn.includes(prompt) ? 1 : 0,
+      cc: i,
+      color,
+    });
+  }
+
+  return prompts;
+}
+
+const DEFAULT_PROMPTS = [
+  { color: '#5C4033', text: 'Luxury Lofi House' },
+  { color: '#ADD8E6', text: 'Crystalline Ambient' },
+  { color: '#B87333', text: 'Tempo ≈85 BPM' },
+  { color: '#8B4513', text: 'Deep Warm Beat' },
+  { color: '#696969', text: 'Vinyl Crackle' },
+  { color: '#E6E6FA', text: 'Glassy Synth Pads' },
+  { color: '#C0C0C0', text: 'Shimmering Bells' },
+  { color: '#F5F5DC', text: 'Simple Melody' },
+  { color: '#483D8B', text: 'Melancholic Mood' },
+  { color: '#D4AF37', text: 'Art-Deco Elegance' },
+  { color: '#20B2AA', text: 'Lush Reverb' },
+  { color: '#FFFFFF', text: 'Clean Production' },
+  { color: '#006A4E', text: 'Sophisticated' },
+  { color: '#B2BEB5', text: 'Reflective' },
+  { color: '#FFBF00', text: 'Crystal & Brass' },
+  { color: '#800020', text: 'Dark Wood Tones' },
+];
+
+main();
